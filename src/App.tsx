b2fe0ecc5068f053eeaa8d144ef4d19483077e9d
lizzykal <72418392+lizzykal@users.noreply.github.com>
2023-09-07 11:25:24 -0400

    Adding image of dog

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
+            <h1>Hello World</h1>;
+            <img
+                src="C:\Users\Lizzy Kalfas\Downloads\Dogimagefortaks3.jpg"
+                alt="A picture of a cute dog"
+            />
             <header className="App-header">
                 UD CISC275 by Lizzy Kalfas Hello World
             </header>
@@ -11,6 +16,11 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>
+                This is just a paragraph of text. It can go onto multiple lines,
+                if you want.
+            </p>
+            ;
         </div>
     );
 }
