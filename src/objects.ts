af3d3263a71c745006d0822ba34f9e575933b68a
lizzykal <72418392+lizzykal@users.noreply.github.com>
2023-09-19 08:36:21 -0400

    Completed isCorrect function

--- src/objects.ts
+++ src/objects.ts
@@ -30,7 +30,13 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    const trimmedAnswer = answer.trim().toLowerCase();
+    const trimmedExpected = question.expected.trim().toLowerCase();
+    if (trimmedAnswer == trimmedExpected) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /**
