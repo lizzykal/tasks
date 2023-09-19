2b21157fa9236e52d77ec0f44518338bbb2de2ba
lizzykal <72418392+lizzykal@users.noreply.github.com>
2023-09-19 08:40:05 -0400

    Completed isValid

--- src/objects.ts
+++ src/objects.ts
@@ -46,7 +46,13 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type == "short_answer_question") {
+        return true;
+    } else if (question.type == "multiple_choice_question") {
+        return question.options.includes(answer);
+    } else {
+        return false;
+    }
 }
 
 /**
