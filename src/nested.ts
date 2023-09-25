ff7bf635ebb5092b1b94a2cab7678c359d9fdabb
lizzykal <72418392+lizzykal@users.noreply.github.com>
2023-09-25 11:38:16 -0400

    Updated sumPoints

--- src/nested.ts
+++ src/nested.ts
@@ -62,7 +62,11 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const totalPoints = questions.reduce(
+        (accumulator, question) => accumulator + question.points,
+        0
+    );
+    return totalPoints;
 }
 
 /***
