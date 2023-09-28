8c019b5d40795b4928839d424bde0bd818c124c7
Lizzy Kalfas <lizzykal@udel.edu>
2023-09-28 11:12:49 -0400

    Completed REavealAnswer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import { Button, Col, Container, Row } from "react-bootstrap";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): JSX.Element {
     return (
         <div className="App">
-<<<<<<< HEAD
             <h1> Lizzy Kalfas </h1>
             <header className="App-header">UD CISC275</header>
             <ol>
@@ -48,7 +44,6 @@ function App(): JSX.Element {
                     </Col>
                 </Row>
             </Container>
-=======
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
@@ -64,7 +59,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
