dda9a917009039a6d64a869f11d7875e59de269a
Lizzy Kalfas <lizzykal@udel.edu>
2023-10-02 22:57:14 -0400

    fixing app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,37 +17,6 @@ function App(): JSX.Element {
         <div className="App">
             <h1> Lizzy Kalfas </h1>
             <header className="App-header">UD CISC275</header>
-            <ol>
-                <li>Dog</li>
-                <li>Cat</li>
-                <li>Hamster</li>
-            </ol>
-            ;
-            <img
-                src="https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half-caption/public/field_blog_entry_images/2021-09/puppy-socialization-pexels-helena-lopes-1790444.jpg?itok=wqs5TWkk"
-                alt="A picture of a cute dog"
-            />
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <Container>
-                <Row>
-                    <Col>
-                        First column
-                        <p>&nbsp;</p>
-                        <svg width="100" height="50">
-                            <rect width="100" height="50" fill="red" />
-                        </svg>
-                    </Col>
-                    <Col>
-                        Second column.
-                        <p>&nbsp;</p>
-                        <svg width="100" height="50">
-                            <rect width="100" height="50" fill="red" />
-                        </svg>
-                    </Col>
-                </Row>
-            </Container>
             <hr></hr>
             {/* <DoubleHalf></DoubleHalf> */}
             <hr></hr>
