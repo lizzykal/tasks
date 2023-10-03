4e297e8be350aad3109199569eb98449a56fd685
Lizzy Kalfas <lizzykal@udel.edu>
2023-10-02 23:04:10 -0400

    fizing app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App(): JSX.Element {
                 </Row>
             </Container>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
